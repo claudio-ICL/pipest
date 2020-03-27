8fd4b7fa22b6e222ba303112d2553b42d9966d17
Claudio Bellani <c.bellani17@imperial.ac.uk>
2020-03-27 16:43:59 +0000

    Add lobster_for_sdhawkes
    Notice that the folder "data/" has been skipped

---
+++ lobster_for_sdhawkes/organise_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ls -1 ./data/ > data/symbols.txt
+
+for sym in "INTC"
+do
+	echo "symbol: $sym"
+	ls ./data/$sym/*message*10*.csv > ./data/$sym/list_of_csv_messagefiles.txt
+	ls ./data/$sym/*orderbook*10.csv > ./data/$sym/list_of_csv_orderbooks.txt
+	echo "list of dates for $sym"
+	awk -F_ '{print $2}' ./data/$sym/list_of_csv_messagefiles.txt | tee ./data/$sym/dates.txt
+	python3 ./py_scripts/produce_dates.py $sym
+done
+
+
