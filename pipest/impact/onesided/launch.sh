b66821499ffd52c02334784799848bd748fa8914
Claudio Bellani <c.bellani17@imperial.ac.uk>
2020-08-18 19:31:10 +0100

    One-sided impact preparation of hpc
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   pipest/impact/onesided/launch.sh
    	modified:   pipest/impact/onesided/main.py
    	modified:   pipest/impact/onesided/wrapper_submission.sh
    
     Changes not staged for commit:
       (use "git add <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   .gitignore
    	modified:   pipest/impact/bacrymuzy/launch.sh
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	pipest/lobster_for_sdhawkes/

--- pipest/impact/onesided/launch.sh
+++ pipest/impact/onesided/launch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 symbol="INTC"
 date="2019-01-23"
-timewindow="45000-48600"
-br="0.15"
-liquidtype="with_the_market"
-controltype="fraction_of_inventory"
+timewindow="37800-41400"
+br="0.10"
 control="0.2"
 
 #action="--read"
@@ -12,12 +10,12 @@ control="0.2"
 #jobid_r=$(qsub -l walltime=05:50:00 -l select=1:ncpus=8:mem=24gb:mpiprocs=1:ompthreads=8 -N "impact-r" -v "$input" wrapper_submission.sh)
 #echo "impact-r submitted with jobid: $jobid_r"
 #action="--measure"
-#input="symbol=$symbol, date=$date, timewindow=$timewindow, action=$action, br=$br, liquidtype=$liquidtype, controltype=$controltype, control=$control"
+#input="symbol=$symbol, date=$date, timewindow=$timewindow, action=$action, br=$br, control=$control"
 #jobid_m=$(qsub -l walltime=05:50:00 -l select=1:ncpus=8:mem=4gb:mpiprocs=1:ompthreads=8 -N "impact-m" -v "$input" -W depend=afterok:$jobid_r wrapper_submission.sh)
 #echo "impact-m submitted with jobid: $jobid_m"
 action="--panmeasure"
 input="symbol=$symbol, date=$date, timewindow=$timewindow, action=$action, br=$br, control=$control"
-jobid_pm=$(qsub -l walltime=17:50:00 -l select=1:ncpus=8:mem=4gb:mpiprocs=1:ompthreads=8 -N "impact-pm" -J 0-5 -v "$input" -W depend=afterok:$jobid_r wrapper_submission.sh)
+jobid_pm=$(qsub -l walltime=17:50:00 -l select=1:ncpus=8:mem=4gb:mpiprocs=1:ompthreads=8 -N "impact-pm" -J 0-7 -v "$input" -W depend=afterok:$jobid_r wrapper_submission.sh)
 echo "impact-pm submitted with jobid: $jobid_pm"
 action="--collect"
 input="symbol=$symbol, date=$date, timewindow=$timewindow, action=$action"
--- pipest/impact/onesided/wrapper_submission.sh
+++ pipest/impact/onesided/wrapper_submission.sh
@@ -3,9 +3,9 @@ module load anaconda3/personal
 source activate h_impact_env
 cd $PBS_O_WORKDIR
 if [ "$action" = "-m" ] || [ "$action" = "--measure" ]; then
-   python main.py  $symbol $date $timewindow $action $br $liquidtype $controltype $control
-elif [ "$action" = "-pm" ] || [ "$action" = "--panmeasure" ]; then
    python main.py  $symbol $date $timewindow $action $br $control
+elif [ "$action" = "-pm" ] || [ "$action" = "--panmeasure" ]; then
+   python main.py  $symbol $date $timewindow $action 
 else
    python main.py  $symbol $date $timewindow $action
 fi
